baf7d9c6e119bdd1a8b71b096457c7963010edd4
koborg <koborg@abv.bg>
2018-02-15 01:18:03 +0200

    first commit. work in progress

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+
+/* eslint-disable no-console */
+console.log(chalk.green('Starts the app in Dev mode.'));
---
+++ buildScripts/testSetup.js
@@ -0,0 +1,5 @@
+require('babel-register')();
+
+//Disable webpack features that Mocha doesn't understand
+
+require.extensions['.css'] = function() {};
